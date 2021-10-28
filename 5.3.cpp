4811ccfd7e76f68273ffa53ada6c554b4f923ccc
Omar228228 <goblock542@gmail.com>
2021-10-28 19:19:43 +0300

    first commit

---
+++ 5.3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double y(const double x); //прототип
+
+int main()
+{
+    double fp, fk, z;
+    int n;
+    cout << "fp = "; cin >> fp;
+    cout << "fk = "; cin >> fk;
+    cout << "n = "; cin >> n;
+    double fg = (fk - fp) / n;
+    double f = fp;
+    while (f <= fk)
+    {
+        z = y(f / 2) + pow(y(f + 1), 2) + y(2 * f);
+        cout << f << " " << z << endl;
+        f += fg;
+    }
+    return 0;
+}
+
+double y(const double x)
+{
+    if (abs(x) >= 1)
+        return (exp(x) / (1 + exp(x)) + sin(x));
+    else
+    {
+        double S = 0;
+        int j = 0;
+        double a = 1;
+        S = a;
+        do
+        {
+            j++;
+            double R = -x * x / ((4 * pow(j, 2)) - (2 * j));
+            a *= R;
+            S += a;
+        } while (j < 8);
+        return S;
+    }
+}
---
+++ UnitTest5.3/UnitTest5.3.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest53
+{
+	TEST_CLASS(UnitTest53)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+				int x = 0;
+				int z;
+				z = (1 + exp(x) + sin(x));
+				Assert::AreEqual(z, 2);
+		}
+	};
+}
